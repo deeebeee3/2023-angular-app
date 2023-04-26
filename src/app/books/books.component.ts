a7fa35202eb772120f317f80d7c6c8885855c60a
Deepak Bhari <deeebeee3@hotmail.com>
2023-04-26 17:38:42 +0100

    Interpolation

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
-  styleUrls: ['./books.component.css']
+  styleUrls: ['./books.component.css'],
 })
 export class BooksComponent {
+  name: string = 'Clean Code';
+  author: string = 'Robert C Martin';
+  src: string =
+    'https://m.media-amazon.com/images/I/41xShlnTZTL._SX376_BO1,204,203,200_.jpg';
 
+  name2: string = 'The Pragmatic Programmer';
+  author2: string = 'David Thomas';
+  src2: string =
+    'https://m.media-amazon.com/images/I/41Suou3DGEL._SX398_BO1,204,203,200_.jpg';
 }
