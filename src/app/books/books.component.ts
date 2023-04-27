3a8530224409c0ebfa80463bea1d2ee961e3a851
Deepak Bhari <deeebeee3@hotmail.com>
2023-04-27 15:15:07 +0100

    NgFor for looping

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,20 +1,31 @@
 import { Component } from '@angular/core';
 
+interface Book {
+  name: string;
+  author: string;
+  image: string;
+}
+
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
   styleUrls: ['./books.component.css'],
 })
 export class BooksComponent {
-  name: string = 'Clean Code';
-  author: string = 'Robert C Martin';
-  src: string =
-    'https://m.media-amazon.com/images/I/41xShlnTZTL._SX376_BO1,204,203,200_.jpg';
-
-  name2: string = 'The Pragmatic Programmer';
-  author2: string = 'David Thomas';
-  src2: string =
-    'https://m.media-amazon.com/images/I/41Suou3DGEL._SX398_BO1,204,203,200_.jpg';
+  books: Book[] = [
+    {
+      name: 'Clean Code',
+      author: 'Robert C Martin',
+      image:
+        'https://m.media-amazon.com/images/I/41xShlnTZTL._SX376_BO1,204,203,200_.jpg',
+    },
+    {
+      name: 'The Pragmatic Programmer',
+      author: 'David Thomas',
+      image:
+        'https://m.media-amazon.com/images/I/41Suou3DGEL._SX398_BO1,204,203,200_.jpg',
+    },
+  ];
 
   isShowing: boolean = true;
 }
